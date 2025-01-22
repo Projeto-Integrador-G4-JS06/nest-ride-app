74d7d01dac41546447c0068af784ee0c1ad9b8c4
Samuel-1210 <samuka2568@gmail.com>
2025-01-22 13:15:11 -0300

    :sparkles: Entidade Veiculo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Veiculo } from './veiculos/entities/veiculo.entity';
+import { VeiculoModule } from './veiculos/veiculos.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
@@ -8,13 +10,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     port: 3306,
     username: 'root',
     password: 'root',
-    database: '', // Cadastrar o nome do BD
-    entities: [], // Cadastrar as Classes Entities, para que o 
+    database: 'db_rides_app', // Cadastrar o nome do BD
+    entities: [Veiculo], // Cadastrar as Classes Entities, para que o 
     // TypeORM possa gerar as tabelas correspondentes no BD
     synchronize: true,
     logging: true
   }),
   // Inserir nome da classe module de cada entidade (ex.: PostagemModule)
+  VeiculoModule,
+
   ],
   controllers: [],
   providers: [],
